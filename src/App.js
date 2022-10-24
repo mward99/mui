2fefcf172b7d44426083ce96767f55687a6df111
mward99 <mike.wardd84@gmail.com>
2022-10-23 21:02:39 -0400

    Added Graph

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import { Button } from "@mui/material";
+import { Chart } from "react-google-charts";
 
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7], // CSS-style declaration
+];
+
+export const options = {
+  title: "My Daily Activities",
+  pieHole: 0.4,
+  is3D: false,
+};
 
 function App() {
   return (
@@ -7,6 +23,14 @@ function App() {
  <Button variant="text">Text</Button>
 <Button variant="contained">Contained</Button>
 <Button variant="outlined">Outlined</Button>
+<Chart
+      chartType="PieChart"
+      width="100%"
+      height="400px"
+      data={data}
+      options={options}
+    />
+
     </div>
   );
 }
